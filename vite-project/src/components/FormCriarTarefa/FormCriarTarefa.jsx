a6d34d713f8ed8673ffa326341ba963f713e52b2
Lhara Raysa <lhararaysa23@gmail.com>
2026-04-13 18:16:02 -0300

    função onChangeNomeTarefa add

--- vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
+++ vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
@@ -1,10 +1,25 @@
-import { CampoTexto,Botao } from "../../components";
+import { useState } from 'react';
+
+import { CampoTexto, Botao } from "../../components";
+
 import style from "./FormCriarTarefa.module.css";
 
-const FormCriarTarefa = () => {
+const FormCriarTarefa = (props) => {
+    const [nomeTarefa, setNomeTarefa] = useState('');
+
+    const { _setTarefas } = props;
+
+    const onChangeNomeTarefa = (event) => {
+        setNomeTarefa(event.currentTarget.value)
+    };
+
+    const _adicionarTarefa = () => {
+
+    };
+
     return (
         <form className={style.FormCriarTarefa}>
-            <CampoTexto/>
+            <CampoTexto value={nomeTarefa} onChange={onChangeNomeTarefa}/>
             <Botao texto="+" />
         </form>
     );
