70c3b6240d38e8c7fa479bf11b3fded0976d1647
Lhara Raysa <lhararaysa23@gmail.com>
2026-04-14 14:20:28 -0300

    feat: exporta componente Loading e corrige formatação do index

--- vite-project/src/components/index.js
+++ vite-project/src/components/index.js
@@ -2,6 +2,7 @@ export * from './Botao'
 export * from './Cabecalho';
 export * from './Conteudo';
 export * from './CampoTexto';
-export * from './Rodape';
 export * from './FormCriarTarefa';
-export * from './ListaTarefas'
+export * from './ListaTarefas';
+export * from './Loading';
+export * from './Rodape';
